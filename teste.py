1853f653f18995065a39fa509e15be61fff33a6f
Giovani Perotto Mesquita <giovanipm@gmail.com>
2020-03-02 18:09:32 -0300

    Giovani_Mesquita(RBST10164) 200302180932

--- teste.py
+++ teste.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 
-def func(a, b):
+def func(a):
     a[0] = 'new-value'     # 'a' references a mutable list
     a[1] = a[1] + 1        # changes a shared object
-    b    = b + 1
 
 args = ['old-value', 99]
-b    = 0
 
-func(args, b)
+func(args)
 
-print(args[0], args[1], b)     # output: new-value 100s
+print(args[0], args[1])     # output: new-value 100s
+
+func(args)
+
+print(args[0], args[1])     # output: new-value 100s
+
+func(args)
+
+print(args[0], args[1])     # output: new-value 100s
+
+func(args)
+
+print(args[0], args[1])     # output: new-value 100s
